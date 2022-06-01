f8647ab5ba5f79fcdc7d4d86df1a38180f6089bd
thomasskk <tkitengec@gmail.com>
2022-06-01 16:33:28 +0200

    export type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-export * from './types/index.js'
+export type {
+  Error,
+  Errors,
+  GetValue,
+  SetValue,
+  UseFormReturn,
+  UseFormRegisterOptions,
+  ValueAs,
+  UseFormRegister,
+  UseFormRegisterReturn,
+} from './types/index.js'
 export * from './useForm.js'
 export * from './useFormContext.js'
