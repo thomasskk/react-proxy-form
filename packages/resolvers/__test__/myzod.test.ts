974d98b2a7c5e9f8f765d5e37d452344ac78cad0
thomasskk <tkitengec@gmail.com>
2022-06-29 23:50:15 +0200

    feat: myzod handle lazy,tuple,literal,union

--- packages/resolvers/__test__/myzod.test.ts
+++ packages/resolvers/__test__/myzod.test.ts
@@ -3,14 +3,120 @@ import { resolver } from '../src/myzod'
 import * as z from 'myzod'
 
 describe('myzod', () => {
-  test('record succeed', () => {
-    const path = 'a.b'
-    const error = resolver(z.object({ a: z.record(z.string()) }))(
+  test('lazy succeed', () => {
+    const path = 'a.[0].a.[0]'
+
+    const schema: any = z.object({
+      a: z.array(z.lazy(() => schema)),
+    })
+
+    const error = resolver(schema)(path, {
+      a: [],
+    }).errors.get(path)
+
+    expect(error).toBeUndefined()
+  })
+  test('lazy fail', () => {
+    const path = 'a.[0].a.[0]'
+
+    const schema: any = z.object({
+      a: z.array(z.lazy(() => schema)),
+    })
+
+    const error = resolver(schema)(path, {
+      a: {},
+    }).errors.get(path)
+
+    expect(error).toBeTruthy()
+  })
+  test('tuple succeed', () => {
+    const path = 'a'
+    const error = resolver(
+      z.object({ a: z.tuple([z.string(), z.object({ a: z.boolean() })]) })
+    )(path, ['', { a: true }]).errors.get(path)
+
+    expect(error).toBeUndefined()
+  })
+  test('tuple fail', () => {
+    const path = 'a'
+    const error = resolver(
+      z.object({ a: z.tuple([z.string(), z.object({ a: z.boolean() })]) })
+    )(path, ['', {}]).errors.get(path)
+
+    expect(error).toBeTruthy()
+  })
+  test('literal succeed', () => {
+    const path = 'a'
+    const error = resolver(z.object({ a: z.literals('a', 'b') }))(
       path,
-      ''
+      'a'
     ).errors.get(path)
 
     expect(error).toBeUndefined()
+  })
+  test('literal fail', () => {
+    const path = 'a'
+    const error = resolver(z.object({ a: z.literals('a', 'b') }))(
+      path,
+      'c'
+    ).errors.get(path)
+
+    expect(error).toBeTruthy()
+  })
+  test('union succeed', () => {
+    const path = 'a.[0]'
+    const error = resolver(
+      z.object({ a: z.union([z.record(z.string()), z.array(z.number())]) })
+    )(path, 1).errors.get(path)
+
+    expect(error).toBeUndefined()
+  })
+  test('union failed', () => {
+    const path = 'a.[0]'
+    const error = resolver(
+      z.object({ a: z.union([z.record(z.string()), z.array(z.number())]) })
+    )(path, {}).errors.get(path)
+
+    expect(error).toBeTruthy()
+  })
+  test('union deep succeed array', () => {
+    const path = 'a.b.[0]'
+    const error = resolver(
+      z.object({
+        a: z.union([
+          z.record(z.union([z.array(z.string()), z.record(z.string())])),
+          z.array(z.number()),
+        ]),
+      })
+    )(path, '').errors.get(path)
+
+    expect(error).toBeUndefined()
+  })
+  test('union deep succeed obj', () => {
+    const path = 'a.b.a'
+    const error = resolver(
+      z.object({
+        a: z.union([
+          z.record(z.union([z.array(z.string()), z.record(z.string())])),
+          z.array(z.number()),
+        ]),
+      })
+    )(path, '').errors.get(path)
+
+    expect(error).toBeUndefined()
+  })
+  test('union deep failed', () => {
+    const path = 'a.b.a'
+    const error = resolver(
+      z.object({
+        a: z.union([
+          z.record(z.union([z.array(z.string()), z.record(z.string())])),
+          z.array(z.number()),
+        ]),
+      })
+    )(path, 1).errors.get(path)
+
+    expect(error).toBeTruthy()
   })
   test('record fail', () => {
     const path = 'a.b'
--- packages/resolvers/src/myzod/index.ts
+++ packages/resolvers/src/myzod/index.ts
@@ -1,28 +1,68 @@
 import * as z from 'myzod'
 
+const findPath: any = (
+  acc: any,
+  cv: string,
+  i: number,
+  value: any,
+  arr: any
+) => {
+  if (!acc) {
+    return
+  }
+  if (Array.isArray(acc?.schemas)) {
+    let err
+
+    for (const schema of acc.schemas) {
+      try {
+        return findPath(schema, cv, i, value, arr)
+      } catch (e) {
+        err = e
+      }
+    }
+    throw err
+  }
+  if (acc?.fn) {
+    return acc.fn(value)
+  }
+
+  if (arr[i + 1]) {
+    if ('schema' in acc) {
+      return acc.schema
+    }
+    if (acc?.objectShape?.schemas) {
+      return acc.objectShape
+    }
+    if (acc?.objectShape?.[z.keySignature]) {
+      return acc.objectShape[z.keySignature]
+    }
+    return acc.objectShape[cv]
+  } else {
+    if (acc?._parse?.name === 'parseRecord') {
+      return acc.parse({ value })
+    }
+    if (acc?._parse?.name === 'parseObjectConv') {
+      return acc.parse(value)
+    }
+    if (acc?.objectShape) {
+      return acc.objectShape[cv].parse(value)
+    }
+    return acc.schema.parse(value)
+  }
+}
+
 export const resolver =
   <T>(schema: T) =>
   (path: string, value: any) => {
     const errors = new Map()
 
     try {
-      path.split('.').reduce((acc: any, cv, i, arr) => {
-        if (cv.charAt(0) === '[' && cv.charAt(cv.length - 1) === ']') {
-          if (arr[i + 1]) {
-            return acc?.schema
-          } else {
-            return acc.schema.parse(value)
-          }
-        }
-        if (arr[i + 1]) {
-          return acc.objectShape[cv]
-        } else {
-          if (acc?._parse?.name === 'parseRecord') {
-            return acc.parse({ value })
-          }
-          return acc.objectShape[cv].parse(value)
-        }
-      }, schema)
+      path
+        .split('.')
+        .reduce(
+          (acc: any, cv, i, arr) => findPath(acc, cv, i, value, arr),
+          schema
+        )
     } catch (err: unknown) {
       if (err instanceof z.ValidationError) {
         errors.set(path, err.message)
